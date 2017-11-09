9f4708df3a1b07bdcd4e1fe69db0b20696085863
fauzanzaid <fauzanzaid.dev@gmail.com>
2017-11-09 19:04:17 +0530

    Add functionality for interaction. Rename functions

--- plotter.py
+++ plotter.py
@@ -4,8 +4,9 @@
 
 import math
 
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider, Button, RadioButtons
 
 
 
@@ -38,19 +39,19 @@ class Plotter():
 		self.obs_coods.append(cood)
 
 
-	def env_plot_grids(self, ax):
+	def plot_grids(self, ax):
 		# X and Y axis
-		ax.axhline(c="0.8")
-		ax.axvline(c="0.8")
+		ax.axhline(c="0.9")
+		ax.axvline(c="0.9")
 
 		# Two circles
-		circle1 = plt.Circle((0,0), self.link_lengths[0], color='0.8', fill=False)
-		circle2 = plt.Circle((0,0), self.link_lengths[0]+self.link_lengths[1], color='0.8', fill=False)
+		circle1 = plt.Circle((0,0), self.link_lengths[0], color='0.9', fill=False)
+		circle2 = plt.Circle((0,0), self.link_lengths[0]+self.link_lengths[1], color='0.9', fill=False)
 		ax.add_artist(circle1)
 		ax.add_artist(circle2)
 
 
-	def env_plot_terminals(self, ax):
+	def plot_terminals(self, ax):
 		ax.plot(*self.start_cood, 'bo')
 		ax.plot(*self.end_cood, 'go')
 		
@@ -58,15 +59,37 @@ class Plotter():
 		ax.annotate(r'$P_t$', xy=self.end_cood, xytext=(5,-10), textcoords='offset points')
 
 
-	def env_plot_obstacles(self, ax):
+	def plot_obstacles(self, ax):
 		for i,cood in enumerate(self.obs_coods):
 			ax.plot(*cood, 'r^')
 			ax.annotate(r'$P_{o'+str(i)+r'}$', xy=cood, xytext=(5,-10), textcoords='offset points')
 
 
-	def env_plot_links(self, ax):
-		coods_x, coods_y = self.get_coods_from_link_angles()
-		ax.plot(coods_x, coods_y, '-mo')
+	def plot_links(self, ax, *args):
+		if args:
+			coods_x, coods_y = args[0]
+		else:
+			coods_x, coods_y = self.get_coods_from_link_angles()
+
+		[link_object] = ax.plot(coods_x, coods_y, '-mo')
+		return link_object
+
+
+	def plot_links_by_time(self, ax, coods_series, t):
+		steps = len(coods_series[0])
+		coods_x = coods_series[0][int(t*(steps-1))]
+		coods_y = coods_series[1][int(t*(steps-1))]
+
+		return self.plot_links(ax, [coods_x, coods_y])
+
+
+	def plot_update_links_by_time(self, link, coods_series, t):
+		steps = len(coods_series[0])
+		coods_x = coods_series[0][int(t*(steps-1))]
+		coods_y = coods_series[1][int(t*(steps-1))]
+
+		link.set_xdata(coods_x)
+		link.set_ydata(coods_y)
 
 
 	def env_set_plot_lims(self, ax):
@@ -76,10 +99,10 @@ class Plotter():
 
 
 	def env_plot(self, ax):
-		self.env_plot_grids(ax)
-		self.env_plot_terminals(ax)
-		self.env_plot_obstacles(ax)
-		self.env_plot_links(ax)
+		self.plot_grids(ax)
+		self.plot_terminals(ax)
+		self.plot_obstacles(ax)
+		self.plot_links(ax)
 		self.env_set_plot_lims(ax)
 
 
@@ -89,20 +112,69 @@ class Plotter():
 		plt.show()
 
 
+	def plot_end_path(self, ax, xs, ys):
+		ax.plot(xs, ys, '--', c="y")
+
+
+	def plot_joint_path(self, ax, xs, ys):
+		ax.plot(xs, ys, '--', c="0.8")
+
+
+	def transition_set_plot_lims(self, ax):
+		ax.axis('scaled')
+		ax.set_xlim(-1.2*self.axis_limit, 1.2*self.axis_limit)
+		ax.set_ylim(-1.2*self.axis_limit, 1.2*self.axis_limit)
+
+
+
 	"""
 	link_angles_series: A time series 2d numpy array. Each element
 		is a link_angles aray, indexed by time
 	"""
-	def plot_path(self, link_angles_series):
-		pass
+	def transition_plot_static(self, ax, coods_series):
+		self.plot_grids(ax)
+		self.plot_terminals(ax)
+		self.plot_obstacles(ax)
+		self.transition_set_plot_lims(ax)
+
+		coods_x_series, coods_y_series = coods_series
+		self.plot_end_path(ax, coods_x_series.T[-1], coods_y_series.T[-1])
+		for xs, ys in zip(coods_x_series.T[:-1], coods_y_series.T[:-1]):
+			self.plot_joint_path(ax, xs, ys)
 
 
-	def get_coods_from_link_angles(self):
+	def transition_show(self, link_angles_series):		
+		fig = plt.figure()
+
+		ax_main = fig.add_axes([0.1,0.2,0.8,0.7])
+		ax_slider = fig.add_axes([0.1,0.03,0.8,0.03])
+
+		coods_series = self.get_coods_series_from_link_angles_series(link_angles_series)
+
+		self.transition_plot_static(ax_main, coods_series)
+		link = self.plot_links_by_time(ax_main, coods_series, 0)
+
+
+		slider = Slider(ax_slider, "Time", 0, 1, valinit=0)
+		def on_slider_upd(val):
+			self.plot_update_links_by_time(link, coods_series, val)
+			fig.canvas.draw()
+		slider.on_changed(on_slider_upd)
+
+		plt.show()
+
+
+	def get_coods_from_link_angles(self, *args):
 		coods_y = [0]
 		coods_x = [0]
 
+		if args:
+			link_angles = args[0]
+		else:
+			link_angles = self.link_angles
+
 		angle = 0
-		for l,a in zip(self.link_lengths, self.link_angles):
+		for l,a in zip(self.link_lengths, link_angles):
 			angle += a 
 			cood_y = coods_y[-1] + l*math.sin(math.radians(angle))
 			cood_x = coods_x[-1] + l*math.cos(math.radians(angle))
@@ -114,4 +186,17 @@ class Plotter():
 
 
 	def get_coods_series_from_link_angles_series(self, link_angles_series):
-		pass
+		coods_y_series = []
+		coods_x_series = []
+		
+		for link_angles in link_angles_series:
+			coods_x, coods_y = self.get_coods_from_link_angles(link_angles)
+			coods_x_series.append(coods_x)
+			coods_y_series.append(coods_y)
+
+		coods_x_series = np.array(coods_x_series)
+		coods_y_series = np.array(coods_y_series)
+		
+		return coods_x_series, coods_y_series
+
+
