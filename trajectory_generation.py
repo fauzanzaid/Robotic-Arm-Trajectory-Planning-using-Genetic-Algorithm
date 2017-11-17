ae45cacdc7169030eb427e1d57f75f3b4379c223
Rishabh Mathur <rmath97@gmail.com>
2017-11-17 16:31:22 +0530

    Envelope function for fitness
    made envelope fiucntion for fitness value calculation and integration with purohit's code

--- trajectory_generation.py
+++ trajectory_generation.py
@@ -1,27 +1,34 @@
 import numpy as np
 import scipy.interpolate as sc
 import matplotlib.pyplot as plt
-
+import invkin
 '''
-generate_trajectories(population, start, end) : 
+generate_trajectories(sorted_population, start, end) : 
     population - matrix of (interlaced x and y coordinates of internal points) of each chromosome of the population. 
                  Each row contains 1 chromosome
     start - x and y coordinates of start point
     end - x and y coordinates of end point
     outputs - list of interpolated functions for each chromosome (list of PchipInterpolator objects)
             - points are also returned.
-    
+format(population):
+    takes a population matrix andconverts into a 3D matrix for better use for generate_trajectories function.
+
+check_point_validity(sorted_population, link1, link2)    
+
 '''
 
 
-def generate_trajectories(sorted_population, start, end):
+def generate_trajectories(sorted_population, start, end, fitness_calculated):
     #Every chromosome's points are seperated and arranged in form of x and y cooridnates. \
     #It is then arranged in the order of x coordinated. Start and End point coordinates are then added to the array.
     #then the trajectories are generated.
     shape = np.shape(sorted_population)
+    left_end
     population_trajectories = []
     trajectory_points = np.zeros([shape[0], shape[1]+2, shape[2]])
     for i in range(shape[0]):
+        if fitness_calculated == True:
+            continue
         ch_with_start = np.insert(sorted_population[i, :, :], 0, start, axis=0)
         chrome_all_pts = np.insert(ch_with_start, (shape[1] + 1), end, axis=0)
         population_trajectories.append(sc.PchipInterpolator(chrome_all_pts[:, 0], chrome_all_pts[:, 1]))
@@ -30,20 +37,30 @@ def generate_trajectories(sorted_population, start, end):
 
 
 def format(population) -> object:
+    '''
+    :param population: complete population in 2D matrix (P x 2k)
+    :return: sorted_population: 3D array (P x k x 2)
+    '''
     shape = np.shape(population)
-    sorted_population = np.zeros([shape[0], int(shape[1]/2), 2])
+    formatted_population = np.zeros([shape[0], int(shape[1]/2), 2])
     for i in range(shape[0]):
         chrome = np.reshape(population[i, :], [int(shape[1]/2), 2])
         chrome_sorted = chrome[chrome[:, 0].argsort()].transpose()
-        sorted_population[i, :, :] = chrome_sorted.transpose()
-    return sorted_population
+        formatted_population[i, :, :] = chrome_sorted.transpose()
+    return formatted_population
 
 
-def check_point_validity(sorted_population, link1, link2) -> list:
-    shape = np.shape(sorted_population)
+def check_point_validity(formatted_population, link1, link2) -> list:
+    '''
+    :param sorted_population: 3D array of sorted population matrix
+    :param link1: length of link 1
+    :param link2: length of link 2
+    :return: validity: list of indexed validity values. could be used for setting fitness to zero.
+    '''
+    shape = np.shape(formatted_population)
     validity = []
     for i in range(shape[0]):
-        r = np.linalg.norm(sorted_population[i, :, :], axis=1)
+        r = np.linalg.norm(formatted_population[i, :, :], axis=1)
         if np.all(r > link1):
             if np.all(r < (link1+link2)):
                 validity.append(True)
@@ -55,6 +72,7 @@ def check_point_validity(sorted_population, link1, link2) -> list:
 
 
 def cleanse_chromosomes(sorted_population, validity):
+    ''' maybe not required '''
     size = len(validity)
     print(sorted_population, '\n', validity)
     clean_population = sorted_population
@@ -64,39 +82,40 @@ def cleanse_chromosomes(sorted_population, validity):
     return clean_population
 
 
-def check_trajectory_validity(trajectories, obstacles):
-    n = np.shape(obstacles)
-    validity = [True for x in range(len(trajectories))]
-    for i in range(n[0]):
-        for path in trajectories:
-                if path(obstacles[i][0]) > obstacles[i][1]:            #value of path at x is greater than y coord of point
-                    validity[path] = False
-                else:
-                    continue
-    print(validity)
+def check_trajectory_validity(trajectory, obstacles):
+    #obstacles are as: [x1, x2, ... xn]
+    #                  [y1, y2, ... yn]
+    #
+    ''' check for purohit's function'''
+    #check for dimensionality while getting changes
 
-    
+    if any(trajectory(obstacles[0]) > obstacles[1]):           #value of path at x is greater than y coord of point
+        validity = False
+    else:
+        validity = True
+    return validity
+
+
 def path_points(y, epsilon, start, end):
-    '''
-    y = PchipInterpolator object for chromosome
-    epsilon = parameter for distance between points
-    start = (x, y) coordinates of start point
-    end = (x, y) coordinates of end point
-    
-    epsilon usage: increasing it will improve resolition at the cost of more points to work on.
+    """
+    :param y: PchipInterpolator object for chromosome
+    :param epsilon: parameter for distance between points
+    :param start: (x, y) coordinates of start point
+    :param end: (x, y) coordinates of end point
+    epsilon usage: increasing it will improve resolution at the cost of more points to work on.
                    decreasing it will improve computation time at the cost of resolution
-    
-    returns (2 x N) array of (X, Y) coordinates of points, where N = no. of points
+
+    :return: (2 x N) array of (X, Y) coordinates of points, where N = no. of points
     (N is variable to accomodate for equal disatnce between consecutive points)
-    '''
+    """
     #temporary lists to store x and y coordinates
     pt_x = [start[0]]
     pt_y = [start[1]]
     der = y.derivative()
-    
+
     #iterator point
     x = start[0]
-    
+
     while (x < end[0]):
         del_x = epsilon/np.sqrt(der(x)**2 +1)
         if (x+del_x) < end[0] :
@@ -107,26 +126,77 @@ def path_points(y, epsilon, start, end):
             pt_x.append(end[0])
             pt_y.append(end[1])
             break
-            
+
     points = np.zeros([2, len(pt_x)])
     points[0, :] = np.array(pt_x)
     points[1, :] = np.array(pt_y)
 
-    return points
+    return points.transpose()
 
 
-def fitness_chromosome(theta, mu):
+def fitness_population(population, link_len, start_pt, end_pt, obstacles, epsilon, mu):
+    """
+    :param
+    envelope function for complete fitness calculation
+    Order of operations:
+    1. point checking       (set fitness to zero for invalid)
+    2. path interpolation
+    3. path discretization
+    4. reverse kinematics on path
+    5. Path checking        (check order here)
+    5. fitness calculation
+    """
+    link1, link2 = link_len[0], link_len[1]
+    arm1 = Arm(link_len)
+
+    pop_size = np.shape(population)[0]
+
+    fitness_pop = [0 for i in range(pop_size)]                       #stores fitness values
+    fitness_calculated = [False for i in range(pop_size)]            #stores fitness calculation validity
+
+    formatted_pop = format(population)
+    pt_validity = check_point_validity(formatted_pop, link1, link2)
+
+    for i in range(len(fitness_calculated)):
+        if pt_validity[i] == False:
+            fitness_pop[i] = 0
+            fitness_calculated[i] = True
+
+    points, trajectories = generate_trajectories(formatted_pop, start_pt, end_pt, fitness_calculated)
+
+    for i in range(pop_size):
+        traj_points = path_points(trajectories[i], epsilon, start_pt, end_pt)
+        #
+        #mystery function by purohit to give me angle values (theta matrix)
+        theta = arm1.time_series(traj_points)
+
+        #mystery function by purohit to give me path validity
+        validity = check_trajectory_validity(trajectories[i], obstacles)
+        if validity == False:
+            fitness_pop[i] = 0
+            fitness_calculated[i] = True
+        else:
+            fitness_pop[i] = fitness_chrome(theta, mu)
+            fitness_calculated[i] = True
+
+    return population, fitness_pop
+
+def fitness_chrome(theta, mu):
+    # check for mu dependency on links
     '''
+    :param theta: 2 x N matrix of link angles at discrete points
+    :param mu: fitness parameter. see inital note for setting mu
+    :return: fitness value of the chromosome
     theta in format of
-    [ th11 th12 th13 th14 ... th1n] 
-    [ th21 th22 th23 th24 ... th2n]
-    theta1 and theta 2 at dicrete pints on the path.
+    [ th11 th12 th13 th14 ... th1n]     link 1 angles
+    [ th21 th22 th23 th24 ... th2n]     link 2 angles
+    theta1 and theta 2 are at discrete points on the path.
     internal variables:
     div = no. of theta divisions, 1 dimension of theta matrix
     '''
     #check this while changing code for different input format
     div = np.shape(theta)[1]
-    
+    theta = theta.T
     theta_i = theta[:, 0:div-2]
     theta_j = theta[:, 1:div-1]
     del_theta = abs(theta_j - theta_i)
@@ -171,9 +241,6 @@ def testing():
     plt.show()
 
 testing()
-
-
-
 
 def radius_bounds(chrome):
     k = np.linalg.norm(chrome, axis=1)
